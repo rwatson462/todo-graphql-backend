6aab327cf6b56f14e183a6eabd518a7605765f60
Rob Watson <robwatson462@gmail.com>
2024-02-24 10:49:24 +0000

    Add Registration, Login, Auth User routes

---
+++ app/GraphQL/Mutations/Login.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use Illuminate\Support\Facades\Auth;
+
+final readonly class Login
+{
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args)
+    {
+        if (Auth::attempt($args)) {
+            /** @var \App\Models\User $user */
+            $user = auth()->user();
+
+            /** @var \Laravel\Sanctum\PersonalAccessToken $token */
+            $token = $user->createToken('api');
+
+            return [
+                'user' => $user,
+                'token' => $token->plainTextToken,
+            ];
+        }
+    }
+}
---
+++ app/GraphQL/Mutations/Register.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+
+final readonly class Register
+{
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args)
+    {
+        if (User::where('email', $args['email'])->exists()) {
+            throw new \Exception("User already exists for this email");
+        }
+        
+        return User::create($args);
+    }
+}
