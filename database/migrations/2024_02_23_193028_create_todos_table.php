b902471b0dc87b585602a35201e616d7c55327e6
Rob Watson <robwatson462@gmail.com>
2024-02-26 14:26:19 +0000

    WIP

---
+++ database/migrations/2024_02_23_193028_create_todos_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->foreignIdFor(User::class)->index();
+
+            $table->string('title');
+            $table->enum('status', ['new', 'complete', 'cancelled'])->default('new');
+
+            $table->timestamps();
+        });
+    }
+};
